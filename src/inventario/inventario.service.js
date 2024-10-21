d1eab88766c746b56a123b11e8f223691f209635
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-21 18:32:42 -0300

    Criação inicial do service

---
+++ src/inventario/inventario.service.js
@@ -0,0 +1,27 @@
+function readAll() {
+  return []
+}
+
+function readById() {
+  
+}
+
+function create() {
+
+}
+
+function updateByID() {
+
+}
+
+function deleteByID() {
+  
+}
+
+module.exports = {
+  readAll,
+  readById,
+  create,
+  updateByID,
+  deleteByID
+}
