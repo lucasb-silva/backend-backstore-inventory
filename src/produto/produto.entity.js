9028e616f344833d8af16560624892ef41b69558
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-25 12:24:09 -0300

    Campo `imagem` alterado para opcional

--- src/produto/produto.entity.js
+++ src/produto/produto.entity.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const produto = Joi.object({
   nome: Joi.string().min(3).max(30).required(),
-  imagem: Joi.string().uri(),
+  imagem: Joi.string().uri().optional(),
   quantidade: Joi.number().min(0).required(),
   descricao: Joi.string().min(3).max(150).required()
 });
