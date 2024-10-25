11905f20152100f7b381942344f0547b91056819
lucasb-silva <lucasbeserrasilva18@gmail.com>
2024-10-25 16:04:53 -0300

    Aumento no tamanho permitido para o campo `descricao`

--- src/produto/produto.entity.js
+++ src/produto/produto.entity.js
@@ -4,7 +4,7 @@ const produto = Joi.object({
   nome: Joi.string().min(3).max(30).required(),
   imagem: Joi.string().uri().optional(),
   quantidade: Joi.number().min(0).required(),
-  descricao: Joi.string().min(3).max(150).required()
+  descricao: Joi.string().min(3).max(600).required()
 });
 
 module.exports = produto
